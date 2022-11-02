04434b212cc5f9fb14e9c574f24c8b0217e207e9
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-02 18:19:38 +0300

    commit tree 200cbb41bc8f6944044aee405ae729c644c23f27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,29 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+
+    delete '/reviews/:id' do
+      review = Review.find(params[:id])
+      review.destroy.to_json
+    end
+
+    post '/reviews' do
+      review = Review.create(
+        comment: params[:comment], 
+        score: params[:score], 
+        user_id: params[:user_id], 
+        game_id: params[:game_id]
+        )
+      review.to_json
+    end
+
+    patch '/reviews/:id' do
+      review = Review.find(params[:id])
+      review.update(
+        comment: params[:comment],
+        score: params[:score]
+        )
+      review.to_json
+    end
+ 
 end
